b50392586f6dab7c5a55287aa7c9ffabd99830c4
salino3 <programacionflavio@gmail.com>
2024-01-31 14:26:01 +0100

    add index.html

--- src/index.ts
+++ src/index.ts
@@ -4,51 +4,20 @@
 // npm install @types/express --save-dev
 
 import express from "express";
-import { getBookList, getBook, insertBook, updateBook, deleteBook } from "./mock-db.js";
+import path from 'path';
+import url from 'url';
+import {booksApi} from './books.api.js';
 
 const app = express();
 
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("My awesome books portal");
-});
-
-app.get("/api/books", async (req, res) => {
-  const bookList = await getBookList();
-  res.send(bookList);
-});
-
-app.get("/api/books/:id", async (req, res) => {
-  const { id } = req.params;
-  const bookId = Number(id);
-  const book = await getBook(bookId);
-  res.send(book);
-});
-
-app.post('/api/books', async (req, res) => {
-
-  const book = req.body;
-  const newBook = await insertBook(book);
-  res.status(201).send(newBook);
-});
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+app.use('/', express.static(path.resolve( __dirname, '../public')));
 
 
-app.put("/api/books/:id", async (req, res) => {
-    const { id } = req.params;
-    const bookId = Number(id);
-  const book = req.body;
-  const newBook = await updateBook(bookId, book);
-  res.status(204).send(newBook);
-});
-
-app.delete("/api/books/:id", async (req, res) => {
-  const { id } = req.params;
-  const bookId = Number(id);
-  await deleteBook(bookId);
-  res.sendStatus(204);
-});
-
+app.use('/api/books', booksApi);
 
 
 app.listen(3000, () => {
